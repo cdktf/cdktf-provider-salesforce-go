7763f88afed01c7d3d9cd25e49ebd2507e392ef0
CDK for Terraform Team <github-team-tf-cdk@hashicorp.com>
2023-06-15 11:33:59 +0000

    chore(release): v5.0.0

--- salesforce/datasalesforceprofile/DataSalesforceProfile.go
+++ salesforce/datasalesforceprofile/DataSalesforceProfile.go
@@ -2,10 +2,10 @@ package datasalesforceprofile
 
 import (
 	_jsii_ "github.com/aws/jsii-runtime-go/runtime"
-	_init_ "github.com/cdktf/cdktf-provider-salesforce-go/salesforce/v4/jsii"
+	_init_ "github.com/cdktf/cdktf-provider-salesforce-go/salesforce/v5/jsii"
 
 	"github.com/aws/constructs-go/constructs/v10"
-	"github.com/cdktf/cdktf-provider-salesforce-go/salesforce/v4/datasalesforceprofile/internal"
+	"github.com/cdktf/cdktf-provider-salesforce-go/salesforce/v5/datasalesforceprofile/internal"
 	"github.com/hashicorp/terraform-cdk-go/cdktf"
 )
 
