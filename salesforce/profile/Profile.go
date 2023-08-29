1208ab893cb7780caefd38608ddf35cfef2b0e56
CDK for Terraform Team <github-team-tf-cdk@hashicorp.com>
2023-08-29 15:01:49 +0000

    chore(release): v6.0.0

--- salesforce/profile/Profile.go
+++ salesforce/profile/Profile.go
@@ -1,11 +1,14 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
 package profile
 
 import (
 	_jsii_ "github.com/aws/jsii-runtime-go/runtime"
-	_init_ "github.com/cdktf/cdktf-provider-salesforce-go/salesforce/v5/jsii"
+	_init_ "github.com/cdktf/cdktf-provider-salesforce-go/salesforce/v6/jsii"
 
 	"github.com/aws/constructs-go/constructs/v10"
-	"github.com/cdktf/cdktf-provider-salesforce-go/salesforce/v5/profile/internal"
+	"github.com/cdktf/cdktf-provider-salesforce-go/salesforce/v6/profile/internal"
 	"github.com/hashicorp/terraform-cdk-go/cdktf"
 )
 
